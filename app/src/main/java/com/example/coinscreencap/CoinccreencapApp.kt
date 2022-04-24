72be280f8dd226f204d4fcc9e86d6467db66c61b
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-04-24 13:15:00 +0430

    add : Appmodule for make hilt

---
+++ app/src/main/java/com/example/coinscreencap/CoinccreencapApp.kt
@@ -0,0 +1,8 @@
+package com.example.coinscreencap
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class CoinccreencapApp : Application()
---
+++ app/src/main/java/com/example/coinscreencap/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.coinscreencap.di
+
+class AppModule {
+}
