9fe3797864058bd463e097e1ddee7f4194b4d07d
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-04-24 13:43:29 +0430

    add : API constants

---
+++ app/src/main/java/com/example/coinscreencap/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.coinscreencap
+
+object constants {
+
+    const val BASE_URL = "https://api.giphy.com/v1/"
+    const val API_KEY = "7BF2jCKZP92NB6HP8vXvMLctg1rl3oCe"
+    const val key = "X-CMC_PRO_API_KEY"
+}
