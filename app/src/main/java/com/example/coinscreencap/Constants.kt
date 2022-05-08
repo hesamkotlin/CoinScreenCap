7cb875281930350326768a1efccead8bd1a33b99
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-05-08 16:08:42 +0430

    edit: Refactor Constans name

--- app/src/main/java/com/example/coinscreencap/Constants.kt
+++ app/src/main/java/com/example/coinscreencap/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.coinscreencap
 
-object constants {
+object Constants {
 
     const val BASE_URL = "https://api.giphy.com/v1/"
     const val API_KEY = "7BF2jCKZP92NB6HP8vXvMLctg1rl3oCe"
