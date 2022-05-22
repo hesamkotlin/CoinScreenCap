378ca0b58e20a017a77949d5eef5187db309fccf
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-05-22 19:05:31 +0430

    add: refactor

--- app/src/main/java/com/example/coinscreencap/data/utils/Extensions.kt
+++ app/src/main/java/com/example/coinscreencap/data/utils/Extensions.kt
@@ -4,25 +4,34 @@ import com.example.coinscreencap.data.database.CoinEntity
 import com.example.coinscreencap.data.model.Crypto
 import com.example.coinscreencap.shared.model.Coin
 
-fun Crypto.mapToCoinEntity(): CoinEntity{
+fun Crypto.mapToCoinEntity(): CoinEntity {
     return CoinEntity(
+        id = id,
         name = name,
         symbol = symbol,
-        cmcRank = cmcRank,
-        price = quote.usd.price,
-        volume24h = quote.usd.volume24h,
-        percentageChange24h = quote.usd.percentChange24h,
+        rank = rank,
+        price = price,
+        volume24h = volume24h,
+        change = change,
+        color = color,
+        marketCap = marketCap,
+        iconUrl = iconUrl,
+        btcPrice = btcPrice,
     )
 }
 
-fun CoinEntity.mapToCoin(): Coin{
+fun CoinEntity.mapToCoin(): Coin {
     return Coin(
         id = id,
         name = name,
         symbol = symbol,
-        cmcRank = cmcRank,
+        rank = rank,
         price = price,
         vol24h = volume24h,
-        percentageChange24h = percentageChange24h,
+        change = change,
+        btcPrice= btcPrice,
+        color = color,
+        iconUrl = iconUrl,
+        marketCap = marketCap
     )
 }
