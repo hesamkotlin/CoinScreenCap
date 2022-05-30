d25575faf3db4af9fc21222c47a33ca29d4272d2
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-05-30 13:41:39 +0430

    add: Observe extention method

--- app/src/main/java/com/example/coinscreencap/shared/model/Extensions.kt
+++ app/src/main/java/com/example/coinscreencap/shared/model/Extensions.kt
@@ -1,3 +1,14 @@
 package com.example.coinscreencap.shared.model
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 
+
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>?, action: (t: T) -> Unit) {
+    liveData?.observe(this) { t -> action(t) }
+}
+
+fun <T> Fragment.observe(liveData: LiveData<T>?, action: (t: T) -> Unit) {
+    viewLifecycleOwner.observe(liveData, action)
+}
